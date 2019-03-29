56d211d2fd5645b35d0336fd0b15b11473be0e48
Arnold C. Sinko <arnoldcsinko@gmail.com>
2019-03-29 19:30:26 -0400

    prep db for deploy

--- db/index.js
+++ db/index.js
@@ -1,17 +1,26 @@
 const knex = require("knex");
-const host = process.env.DB_HOST;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB;
-
-const db = knex({
-	client: "pg",
-	connection: {
+let connectionConfig = {};
+if (process.env.DATABASE_URL) {
+	connectionConfig = {
+		host: process.env.DATABASE_URL,
+		ssl: true
+	};
+} else {
+	const host = process.env.DB_HOST;
+	const user = process.env.DB_USER;
+	const password = process.env.DB_PASSWORD;
+	const database = process.env.DB;
+	connectionConfig = {
 		host: host,
 		user: user,
 		password: password,
 		database: database
-	}
+	};
+}
+
+const db = knex({
+	client: "pg",
+	connection: connectionConfig
 });
 
 module.exports = {
